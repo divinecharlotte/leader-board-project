7224a5982232d9bd85e0aa974cc3e15ba01a9212
charlotte-divine <divinemaina@gmail.com>
2022-06-15 15:16:15 +0100

    fetch data from API

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ const newScore = new Score();
 const addNewScore = document.querySelector('.add-new-score');
 addNewScore.addEventListener('submit', (e) => {
   e.preventDefault();
-  const name = addNewScore.name.value;
+  const user = addNewScore.name.value;
   const scoreNum = addNewScore.score.value;
-  newScore.addScore({ name, scoreNum });
+  newScore.addScore({ user, scoreNum });
   addNewScore.name.value = '';
   addNewScore.score.value = '';
 });
-window.addEventListener('load', newScore.getScore);
+
+const refreshButton = document.querySelector('.refresh-button');
+refreshButton.addEventListener('click', newScore.getScore);
+window.addEventListener('load', newScore.fetchScore);
