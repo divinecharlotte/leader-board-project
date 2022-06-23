c5acc5f53bb9c331fe696f4eeee6ae4715dbd914
charlotte-divine <divinemaina@gmail.com>
2022-06-23 16:19:04 +0100

    deploy the page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
+    bundle: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
